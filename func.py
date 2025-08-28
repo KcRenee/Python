02afd7b003808594ad0c3ea67393e8d68c27be00
renee-11 <kcrenee42@gmail.com>
2025-08-28 23:09:11 +0100

    List of functions List in py Variables Dictionaries for and while loop

--- func.py
+++ func.py
@@ -1,23 +1,51 @@
+lists = [12, 45, 25, 32, 22, 87, 99]
+names = ['Sophia', 'James', 'Vera', 'Daniella', 'Monica', 'Rachel']
+school = 'Miva'
+Luxury = {
+    1: 'Chanel',
+    2: 'LV',
+    3: 'Prada',
+    4: ['YSL', 'Cartier']
+}
+
+
+# List to a function
+
+
 def count(lst):
     even = 0
     odd = 0
     for i in lst:
-        if i % 2 == 0:          
+        if i % 2 == 0:
             even += 1
         else:
             odd += 1
     return even, odd
 
 
-lists = [12, 45, 25, 32, 22, 87, 99]
 evens, odds = count(lists)
 print("There are {} even numbers and {} odd numbers" .format(evens, odds))
 
-
-names = ['Sophia', 'James', 'Vera', 'Daniella']
-names.insert(3, 55)
+# List in python
+names.insert(3, 'Zoey')
+lists.sort()
+print(lists)
 print(names)
 
-
-school = 'Miva'
+# Variables
 print(id(school))
+
+# Dictionaries
+print(Luxury)
+age = dict(zip(names, lists))
+print(age)
+
+# for loop
+for j in Luxury.values():
+    print(j)
+
+# while loop
+k = 1
+while k <= 4:
+    print(school)
+    k = k + 1
